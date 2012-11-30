e3255d931b00e24501407a4bdc08e16d8e74d6bc
Xananax <xananax@yelostudio.com>
2012-11-30 06:08:00 +0200

    added permissions

--- code/LinkExternal.php
+++ code/LinkExternal.php
@@ -102,9 +102,60 @@ class LinkExternal extends DataObject{
 		return '<a href="'.$this->URL.'" class="'.$this->HTMLClasses().'" target="_blank" title="'.$this->Text.'">'.str_replace(array('http://','https://'),'',$this->URL).'</a>';
 	}
 
-	public function canEdit(){return true;}
-	public function canCreate(){return true;}
-	public function canDelete(){return true;}
-	public function canPublish(){return true;}
-	
+	protected function _providePermissionsArray($c=null){
+		if(!$c){$c = $this->class;};
+		$perms = array();
+		$titles = array(
+			'CREATE'=> 'Create'
+		,	'VIEW'	=> 'View'
+		,	'EDIT'=> 'Edit'
+		,	'DELETE'=> 'Delete'
+		,	'PUBLISH'=> 'Publish'
+		);
+		foreach ($titles as $key => $value) {
+			$name = $c.'_'.$key;
+			$niceName = ucfirst($c);
+			$perms[$name] = array(
+				'name' => _t(
+					'Permission.'.$name,
+					$value.' '.$niceName
+				)
+			,	'category' => _t(
+					'Permission.CATEGORY_'.$c,
+					$niceName
+				)
+			,	'help' => _t(
+					'Permission.'.$name.'_HELP',
+					'Allows the user to '.$value.' '.$niceName 
+				)
+			,	'sort' => 100
+			);
+		}
+		return $perms;
+	}
+
+	public function providePermissions(){
+		return $this->_providePermissionsArray($this->class);
+	}
+
+	public function canEdit(){
+		return Permission::check($this->class.'_EDIT');
+	}
+
+	public function canCreate(){
+		return Permission::check($this->class.'_CREATE');
+	}
+
+	public function canDelete(){
+		return Permission::check($this->class.'_DELETE');
+	}
+
+	public function canPublish(){
+		return Permission::check($this->class.'_PUBLISH');
+	}
+
+	public function canView(){
+		return true;
+		//return Permission::check($this->class.'_VIEW');
+	}
 }
